8f471d148c667ff7d6ada77e8920bc75bd3b5761
ealpha072 <ealpha072@gmail.com>
2021-01-03 23:18:08 +0300

    Table modification

--- readuser.php
+++ readuser.php
@@ -3,10 +3,10 @@
   <form action="" method="post">
     <div class="form-group">
       <label for="location">Location</label>
-      <input type="text" class="form-control" id="location" placeholder="Search by location">
+      <input type="text" class="form-control" id="location" placeholder="Search by location" name='location'>
     </div>
     <div class="form-group">
-      <button class="btn btn-primary">View Results</button>
+      <button class="btn btn-primary" name= "submit-location">View Results</button>
   </div>
   <a href="index.php">Back to home</a>
   </form>
@@ -16,7 +16,10 @@
         <th scope="col">#</th>
         <th scope="col">First</th>
         <th scope="col">Last</th>
-        <th scope="col">Handle</th>
+        <th scope="col">E-mail</th>
+        <th scope="col">Location</th>
+        <th scope="col">Age</th>
+        <th scope="col">Date</th>  
       </tr>
     </thead>
     <tbody>
