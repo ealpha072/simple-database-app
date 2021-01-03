e9dd59661d9e9ea04d1eab068711066ee70dc160
ealpha072 <ealpha072@gmail.com>
2021-01-03 17:19:29 +0300

    Included footer and header files

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
-
+<?php include "header.php";?>
 <div class="actions">
-    <ul>
-      <li><a href="">Create</a> -add new user</li>
-      <li><a href="">Read</a>- find a user</li>
-    </ul>
-  </div>
+  <ul>
+    <li><a href="">Create</a> -add new user</li>
+    <li><a href="">Read</a>- find a user</li>
+  </ul>
+</div>
+<?php include "footer.php";?>
