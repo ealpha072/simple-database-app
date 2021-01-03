e45483f92a6948237f096150bf79d17d491b32f6
ealpha072 <ealpha072@gmail.com>
2021-01-03 22:12:24 +0300

    Name attributes for inputs

--- createuser.php
+++ createuser.php
@@ -1,28 +1,42 @@
 <?php include "header.php";?>
+
+<?php
+  $firstname = $lastname = $email = $location = $age = '';
+
+  if (isset($_POST['submit'])) {
+    require "config.php";
+    $mysql  = "INSERT INTO users (firstname,lastname,email,age,location) values ('')"
+
+  }
+
+
+
+
+?>
   <form action="" method = 'post'>
-  <div class="form-group">
-    <label for="firstname">First Name</label>
-    <input type="text" class="form-control" id="firstname" placeholder="First Name">
-  </div>
-  <div class="form-group">
-    <label for="lastname">Last Name</label>
-    <input type="text" class="form-control" id="lastname" placeholder="Another ">
-  </div>
-  <div class="form-group">
-    <label for="email">Email Address</label>
-    <input type="email" class="form-control" id="email" placeholder="Email Address ">
-  </div>
-  <div class="form-group">
-    <label for="age">Age</label>
-    <input type="number" class="form-control" id="age" placeholder="Age">
-  </div>
-  <div class="form-group">
-    <label for="location">Location</label>
-    <input type="text" class="form-control" id="location" placeholder="Location">
-  </div>
-  <div class="form-group">
-    <button class="btn btn-primary" name="submit">Submit</button>
-  </div>
+    <div class="form-group">
+      <label for="firstname">First Name</label>
+      <input type="text" class="form-control" id="firstname" placeholder="First Name" name="firstname">
+    </div>
+    <div class="form-group">
+      <label for="lastname">Last Name</label>
+      <input type="text" class="form-control" id="lastname" placeholder="Another " name="lastname">
+    </div>
+    <div class="form-group">
+      <label for="email">Email Address</label>
+      <input type="email" class="form-control" id="email" placeholder="Email Address " name="email">
+    </div>
+    <div class="form-group">
+      <label for="age">Age</label>
+      <input type="number" class="form-control" id="age" placeholder="Age" name="age">
+    </div>
+    <div class="form-group">
+      <label for="location">Location</label>
+      <input type="text" class="form-control" id="location" placeholder="Location" name="location">
+    </div>
+    <div class="form-group">
+      <button class="btn btn-primary" name="submit">Submit</button>
+    </div>
   </form>
 
   <a href="index.php">Back to home</a>
