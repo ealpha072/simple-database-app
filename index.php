6daddba4174bc5e4c7317accc5a453ef0bc82752
ealpha072 <ealpha072@gmail.com>
2021-01-03 17:16:18 +0300

    Linked bootstrap

--- index.php
+++ index.php
@@ -1,21 +1,7 @@
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv = "x-ua-compatible" content="ie=edge"/>
-  <title>Simple Database App</title>
-</head>
-<body>
- <div class="container">
-  <div class="head">
-    <h1>Simple Database App</h1>
-  </div>
-  <div class="actions">
+
+<div class="actions">
     <ul>
       <li><a href="">Create</a> -add new user</li>
       <li><a href="">Read</a>- find a user</li>
     </ul>
   </div>
- </div>
-</body>
-</html>
