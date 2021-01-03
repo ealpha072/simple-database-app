c42e1a976f4a1aa005e9611e22b7e7f71d47f3b1
ealpha072 <ealpha072@gmail.com>
2021-01-03 17:13:52 +0300

    Moved header and footer to different php files

---
+++ header.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv = "x-ua-compatible" content="ie=edge"/>
+  <title>Simple Database App</title>
+</head>
+<body>
+ <div class="container">
+  <div class="head">
+    <h1>Simple Database App</h1>
+  </div>
+  
+ 
