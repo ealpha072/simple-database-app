06d2f50cfe682ad5b682e91398895778d3598f3e
ealpha072 <ealpha072@gmail.com>
2021-01-04 12:22:01 +0300

    Read user href attribute added

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <div class="actions">
   <ul>
     <li><a href="createuser.php">Create</a> -add new user</li>
-    <li><a href="">Read</a>- find a user</li>
+    <li><a href="readuser.php">Read</a>- find a user</li>
   </ul>
 </div>
 <?php include "footer.php";?>
