a748853f6dce168c276311769a89c4c5dcfcf371
ealpha072 <ealpha072@gmail.com>
2021-01-04 15:45:58 +0300

    Completed the delete process

--- delete.php
+++ delete.php
@@ -9,11 +9,50 @@
   if(isset($_GET['id'])){
     $id = $_GET['id'];
 
-    $delquery = "DELETE * FROM users WHERE id ='$id' ";
+    $delquery = "DELETE FROM users WHERE id ='$id' ";
     $results = mysqli_query($conn,$delquery);
+    #$success = "User deleted successfully!!";
+
+
   }
+  $displayquery = "SELECT * FROM users";
+  $results = mysqli_query($conn, $displayquery);
+  $rows = mysqli_fetch_all($results, MYSQLI_ASSOC);
+  #echo $success;
+
+
+  
 ?>
 
-<?php require "headere.php"; ?>
+<?php require "header.php"; ?>
 
 <h2>Delete users</h2>
+
+<table class="table table-striped table-dark">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First Name</th>
+        <th scope="col">Last</th>
+        <th scope="col">E-mail</th>
+        <th scope="col">Location</th>
+        <th scope="col">Age</th>
+        <th scope="col">Date</th> 
+        <th scope="col" span-col='2' class='text-center'>Action</th> 
+      </tr>
+    </thead>
+    <tbody>
+          <?php foreach($rows as $row){?>
+          <tr>
+            <td><?php echo $row['id'];?></td>
+            <td><?php echo $row['firstname'];?></td>
+            <td><?php echo $row['lastname'];?></td>
+            <td><?php echo $row['email'];?></td>
+            <td><?php echo $row['location'];?></td>
+            <td><?php echo $row['age'];?></td>
+            <td><?php echo $row['date'];?></td>
+            <td><a href="delete.php?id=<?php echo $row['id'];?>">Delete</a></td>
+          </tr>
+          <?php }?>
+    </tbody>
+  </table>
