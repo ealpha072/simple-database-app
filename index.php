2761c0809e69e1b43b119b848bb0fa3b73cff46a
ealpha072 <ealpha072@gmail.com>
2021-01-04 12:49:19 +0300

    Update link added

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <ul>
     <li><a href="createuser.php">Create</a> -add new user</li>
     <li><a href="readuser.php">Read</a>- find a user</li>
+    <li><a href="update.php">Update</a>- edit a user</li>
   </ul>
 </div>
 <?php include "footer.php";?>
---
+++ update.php
@@ -0,0 +1,41 @@
+<?php require "header.php";?>
+<?php include "config.php";?>
+
+<h2>Update users</h2>
+<?php 
+  $sql = "SELECT * FROM users";
+  $results = mysqli_query($conn,$sql);
+  
+  if(mysqli_num_rows($results) > 0){
+    $rows = mysqli_fetch_all($results, MYSQLI_ASSOC); ?>
+    
+    <table class="table table-striped table-dark">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First Name</th>
+        <th scope="col">Last</th>
+        <th scope="col">E-mail</th>
+        <th scope="col">Location</th>
+        <th scope="col">Age</th>
+        <th scope="col">Date</th>  
+      </tr>
+    </thead>
+    <tbody>
+          <?php foreach($rows as $row){?>
+          <tr>
+            <td><?php echo $row['id'];?></td>
+            <td><?php echo $row['firstname'];?></td>
+            <td><?php echo $row['lastname'];?></td>
+            <td><?php echo $row['email'];?></td>
+            <td><?php echo $row['location'];?></td>
+            <td><?php echo $row['age'];?></td>
+            <td><?php echo $row['date'];?></td>
+          </tr>
+          <?php }?>
+    </tbody>
+  </table>
+  <?php } else {?>
+    <h3>No results found for location <?php echo $_POST['location'];?></h3>
+  <?php }?>
+
